179c70ea9b2833801cab5c777d43678863abf2b0
annay555 <annadreams1988@gmail.com>
2026-05-25 09:59:07 +0100

    Add baseURL

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://practicesoftwaretesting.com',
 
     /* Use a custom test id attribute for locator helpers. */
     testIdAttribute: 'data-test',
